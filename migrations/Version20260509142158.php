970232ecb8ffa609295c59afc13a30e1664b5613
Anis Lafrad <e22500013@univ-brest.fr>
2026-05-10 14:29:07 +0200

    Phase 0.1 : ajout d'une cmd pour hash les mdp manuellement + openssl

--- migrations/Version20260509142158.php
+++ migrations/Version20260509142158.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260506092834 extends AbstractMigration
+final class Version20260509142158 extends AbstractMigration
 {
     public function getDescription(): string
     {
